e8ec4379da9e2b2c505039ed8a5fbc4ac875d376
prkr28 <priyanshu402muz@gmail.com>
2025-06-21 17:25:10 +0530

    utils updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("server is running");
+    });
+  })
+  .catch((err) => {
+    console.log("db connection failed", err);
+  });
